1eb07f8484caf8c2da40a42bf4b34cd012c0e589
Saurav Bhattacharya <online.saurav@gmail.com>
2026-02-13 21:17:14 -0800

    Add example usage and comments to factor.ml and list_last_elem.ml

--- factor.ml
+++ factor.ml
@@ -1,3 +1,6 @@
+(* Prime factorization using recursive trial division *)
+(* Demonstrates: recursion, pattern matching, input validation *)
+
 (* Recall that d divides n iff [n mod d = 0] *)
 let factors n =
   if n < 2 then invalid_arg "factors: input must be >= 2"
@@ -8,3 +11,14 @@ let factors n =
       else aux (d + 1) n
     in
     aux 2 n;;
+
+(* Pretty-print a list of integers *)
+let string_of_int_list lst =
+  "[" ^ String.concat "; " (List.map string_of_int lst) ^ "]";;
+
+(* Example usage *)
+let () =
+  let test_values = [2; 12; 30; 97; 360] in
+  List.iter (fun n ->
+    Printf.printf "factors %d = %s\n" n (string_of_int_list (factors n))
+  ) test_values;;
--- list_last_elem.ml
+++ list_last_elem.ml
@@ -1,4 +1,17 @@
+(* Find the last element of a list using pattern matching *)
+(* Demonstrates: option types, pattern matching, recursion *)
+
 let rec last = function
 	| [] -> None
 	| [x] -> Some x
 	| _:: t -> last t;;
+
+(* Example usage *)
+let () =
+  let print_result name = function
+    | None -> Printf.printf "last %s = None\n" name
+    | Some x -> Printf.printf "last %s = Some %d\n" name x
+  in
+  print_result "[1; 2; 3]" (last [1; 2; 3]);
+  print_result "[42]" (last [42]);
+  print_result "[]" (last []);;
